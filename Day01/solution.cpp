844852b672203510deceb7d7e6ec176a82e367d7
Diogo Dores <diogo.dores@gmail.com>
2021-12-01 11:19:45 +0000

    Day 1 solution - Part 1 and 2

--- Day01/solution.cpp
+++ Day01/solution.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-   ifstream file("../Day01/example.txt");
+int partOne(vector<int> input){
+   int lastNumber = input[0], result = 0;
+
+   for(int i = 1; i < input.size(); i++){
+      if(input[i] > lastNumber){
+         result++;
+      }
+      lastNumber = input[i];
+   }
+
+   return result;
+}
+
+int partTwo(vector<int> input){
+   int window = 3, lastSum = 0, result = 0;
+
+   for (int i = 0; i < input.size() - window + 1; i++) {
+      int currentSum = 0;
+      for (int j = 0; j < window; j++)
+         currentSum += input[i + j];
+
+      if (currentSum > lastSum)
+         result++;
+
+      lastSum = currentSum;
+   }
+
+   //Subtracts the first window comparison with 0
+   return result - 1;
+}
+
+vector<int> getInput(string filePath){
+   ifstream file(filePath);
+   vector<int> input;
+
    if (file.is_open()) {
       string line;
-      while (getline(file, line)) {
-         cout << line << endl;
-      }
-      file.close();
+      while (getline(file, line))
+         input.push_back(stoi(line));
    }
+   file.close();
+   return input;
+}
+
+int main()
+{
+   vector<int> input = getInput("../Day01/input.txt");
+   cout << "Part 1: " << partOne(input) << endl;
+   cout << "Part 2: " << partTwo(input) << endl;
 }
